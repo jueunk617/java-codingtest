4e28b90f8dcb7d7351bcdaf72f22ed7a40798628
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2024-06-28 00:17:07 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+// 자연수: A+B, A-B, A*B, A/B(몫), A%B
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
