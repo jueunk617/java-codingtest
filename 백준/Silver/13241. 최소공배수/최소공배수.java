22ce504b0b1a48e725fc614aab1a89fd75cea2b8
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2025-04-14 20:12:45 +0900

    [Silver V] Title: 최소공배수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static long gcd(long a, long b) {
+        while(b != 0) {
+            long tmp = a % b;          
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+    
+    public static long lcm(long a, long b) {
+        return a*b / gcd(a, b);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+       
+        Long A = Long.parseLong(st.nextToken());
+        Long B = Long.parseLong(st.nextToken());
+        
+        System.out.println(lcm(A, B));
+       
+    }
+}
