21a1e707324ca1f469d65903acfaffc0eb507e37
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2025-04-19 21:48:30 +0900

    [Silver IV] Title: 스택 2, Time: 976 ms, Memory: 234996 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -5,6 +5,8 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
         int N = Integer.parseInt(br.readLine());
         
         Stack<Integer> stack = new Stack<>();
@@ -14,44 +16,38 @@ public class Main {
             
             int command = Integer.parseInt(st.nextToken());
             
-            switch(command) {
-                case 1 :
+            switch (command) {
+                case 1:
                     int X = Integer.parseInt(st.nextToken());
                     stack.push(X);
                     break;
                     
-                case 2 :
-                    if(!stack.isEmpty()) {
-                        System.out.println(stack.pop());
-                        break;
+                case 2:
+                    if (!stack.isEmpty()) {
+                        sb.append(stack.pop()).append('\n');
                     } else {
-                        System.out.println("-1");
-                        break;
+                        sb.append("-1\n");
                     }
-                    
-                case 3 :
-                    System.out.println(stack.size());
                     break;
                     
-                case 4 :
-                    if(stack.isEmpty()) {
-                        System.out.println("1");
-                        break;
-                    } else {
-                        System.out.println("0");
-                        break;
-                    }
+                case 3:
+                    sb.append(stack.size()).append('\n');
+                    break;
                     
-                case 5 :
-                    if(!stack.isEmpty()) {
-                        System.out.println(stack.peek());
-                        break;
-                    } else {
-                        System.out.println("-1");
-                        break;
-                    }
+                case 4:
+                    sb.append(stack.isEmpty() ? "1\n" : "0\n");
+                    break;
                     
+                case 5:
+                    if (!stack.isEmpty()) {
+                        sb.append(stack.peek()).append('\n');
+                    } else {
+                        sb.append("-1\n");
+                    }
+                    break;
             }
         }
+        
+        System.out.print(sb);
     }
 }
