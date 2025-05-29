d0fb8f06ba6f7d2545ef66c02e940d170e9f46c6
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2025-05-29 11:54:58 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.73 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,18 +1,7 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i=1; i<=n; i++) {
-            if (i % 2 != 0) list.add(i);
-        }
-        
-        int[] arr = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            arr[i] = list.get(i);
-        }
-        
-        return arr;
+        return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).toArray();
     }
 }
