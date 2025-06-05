9342e97f1453e89e17899fd566fab8c64386d34a
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2025-06-05 16:23:58 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        String[] arr = String.valueOf(x).split("");
+        int sum = 0;
+        
+        for(String s : arr) {
+            sum += Integer.parseInt(s);
+        }
+
+        return (x % sum == 0) ? true : false;
+        
+    }
+}
