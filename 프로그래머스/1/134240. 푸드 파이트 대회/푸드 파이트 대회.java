06362112893319ee1173f49396a59e189d901731
jueunk617 <125340502+jueunk617@users.noreply.github.com>
2025-06-16 23:48:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.66 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,15 +2,13 @@ class Solution {
     public String solution(int[] food) {
         StringBuilder left = new StringBuilder();
         
+        // repeat() 활용 ver
         for (int i=1; i<food.length; i++) {            
-            for (int j=0; j<food[i]/2; j++) {
-                left.append(i);
-            }
+            left.append(String.valueOf(i).repeat(food[i]/2));
         }
         
         StringBuilder answer = new StringBuilder();
-        answer.append(left);
-        answer.append(0);
+        answer.append(left + "0");
         answer.append(left.reverse());
         
         return answer.toString();
